d3c9bd9f15ebc1a412903a591f8142dac3adf82f
HAMMAL8 <Hammal07045@gmail.com>
2025-12-17 00:48:52 +0100

    Add MongoDB connection setup in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,6 @@
+const mongoose = require("mongoose");
 
+module.exports = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/hammalmessage");
+  console.log("✅ MongoDB connected");
+};
