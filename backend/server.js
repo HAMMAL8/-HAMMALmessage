5892463d2ee606958b605aaa0616124234839f70
HAMMAL8 <Hammal07045@gmail.com>
2025-12-17 05:45:50 +0100

    Set up Express server with Socket.io and MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const connectDB = require("./config/db");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+connectDB();
+require("./socket")(io);
+
+server.listen(5000, () =>
+  console.log("🚀 Backend running on http://localhost:5000")
+);
