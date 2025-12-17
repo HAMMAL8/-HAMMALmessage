96f7f46de90b9d759a59fd4c3132a263a10801ef
HAMMAL8 <Hammal07045@gmail.com>
2025-12-17 05:44:43 +0100

    Add socket connection and messaging functionality
    Implement socket connection handling for user status and messaging.

--- backend/socket.js
+++ backend/socket.js
@@ -1,1 +1,35 @@
+const Message = require("./models/Message");
+const User = require("./models/User");
 
+module.exports = (io) => {
+  io.on("connection", (socket) => {
+    console.log("🟢 User connected:", socket.id);
+
+    socket.on("join", async (username) => {
+      socket.username = username;
+      socket.join(username);
+      await User.findOneAndUpdate(
+        { username },
+        { online: true },
+        { upsert: true }
+      );
+      io.emit("status");
+    });
+
+    socket.on("send", async (data) => {
+      const msg = await Message.create(data);
+      io.to(data.receiver).emit("receive", msg);
+    });
+
+    socket.on("disconnect", async () => {
+      if (socket.username) {
+        await User.updateOne(
+          { username: socket.username },
+          { online: false }
+        );
+        io.emit("status");
+      }
+      console.log("🔴 User disconnected");
+    });
+  });
+};
