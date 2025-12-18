63f6c340ae796dcf598c1f546df18a6b5652a5ae
HAMMAL8 <Hammal07045@gmail.com>
2025-12-18 21:30:04 +0100

    Add group reference to Message schema

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -4,7 +4,8 @@ const MessageSchema = new mongoose.Schema({
   sender: String,
   receiver: String,
   text: String,
+  group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Message", MessageSchema);
+module.exports = mongoose.model("Message", MessageSchema); 
