3bc1e87a37eacf72f22c348ec1e08c1f81ea1aa6
HAMMAL8 <Hammal07045@gmail.com>
2025-12-18 22:36:15 +0100

    Implement user presence tracking with socket events

--- backend/socket/presence.js
+++ backend/socket/presence.js
@@ -1,1 +1,16 @@
+const User = require("../models/User");
 
+module.exports = (io, socket) => {
+  socket.on("online", async userId => {
+    socket.userId = userId;
+    await User.findByIdAndUpdate(userId, { online: true });
+    io.emit("presence", { userId, online: true });
+  });
+
+  socket.on("disconnect", async () => {
+    if (socket.userId) {
+      await User.findByIdAndUpdate(socket.userId, { online: false });
+      io.emit("presence", { userId: socket.userId, online: false });
+    }
+  });
+};
