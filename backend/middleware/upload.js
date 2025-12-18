23a3528da8c4be211c50b9e1c2e8f1d64e8039a3
HAMMAL8 <Hammal07045@gmail.com>
2025-12-18 22:26:39 +0100

    Implement multer middleware for Cloudinary uploads

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,1 +1,13 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../services/cloudinary");
 
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "hammalmessage",
+    resource_type: "auto"
+  }
+});
+
+module.exports = multer({ storage });
