afeac570d954643ff2c0ed69e2d698354d3de9ab
HAMMAL8 <Hammal07045@gmail.com>
2025-12-18 21:24:25 +0100

    Add Group model with schema definition

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const GroupSchema = new mongoose.Schema({
+  name: String,
+  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+});
+
+module.exports = mongoose.model("Group", GroupSchema);
