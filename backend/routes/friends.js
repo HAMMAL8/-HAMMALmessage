eebfa25dce645228f8ffc3f589587e6784db7069
HAMMAL8 <Hammal07045@gmail.com>
2025-12-18 21:31:19 +0100

    Implement friend request and acceptance routes

--- backend/routes/friends.js
+++ backend/routes/friends.js
@@ -1,1 +1,30 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const FriendRequest = require("../models/FriendRequest");
+const auth = require("../middleware/authMiddleware");
 
+// Send request
+router.post("/request/:id", auth, async (req, res) => {
+  await FriendRequest.create({ from: req.user.id, to: req.params.id });
+  res.json({ msg: "Request sent" });
+});
+
+// Accept request
+router.post("/accept/:id", auth, async (req, res) => {
+  const reqDoc = await FriendRequest.findById(req.params.id);
+  reqDoc.status = "accepted";
+  await reqDoc.save();
+
+  await User.findByIdAndUpdate(reqDoc.from, { $push: { friends: reqDoc.to } });
+  await User.findByIdAndUpdate(reqDoc.to, { $push: { friends: reqDoc.from } });
+
+  res.json({ msg: "Friend added" });
+});
+
+// Get friends
+router.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user.id).populate("friends");
+  res.json(user.friends);
+});
+
+module.exports = router;
