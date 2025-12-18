f5481b78e6e35fc933ad5fa63940ebb512392be1
HAMMAL8 <Hammal07045@gmail.com>
2025-12-18 21:33:20 +0100

    Implement group messaging functionality

--- backend/socket/chat.js
+++ backend/socket/chat.js
@@ -1,1 +1,15 @@
+const Message = require("../models/Message");
 
+module.exports = (io, socket) => {
+  socket.on("join-group", groupId => socket.join(groupId));
+
+  socket.on("group-message", async data => {
+    const msg = await Message.create({
+      sender: socket.userId,
+      group: data.groupId,
+      text: data.text
+    });
+
+    io.to(data.groupId).emit("group-receive", msg);
+  });
+};
